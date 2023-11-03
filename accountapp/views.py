424fb5f411b16b75a22eab6be78e5ecaa4b948ef
sungno <ysn3939@gmail.com>
2023-11-03 14:50:25 +0900

    Bug Fix

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, DeleteView, UpdateView
 
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
@@ -37,8 +37,17 @@ class AccountDetailView(DetailView):
     context_object_name = "target_user"
     template_name = 'accountapp/detail.html'
 
-class AccountUpdateView(CreateView):
+
+class AccountUpdateView(UpdateView):
     model = User
+    context_object_name = "target_user"
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = "target_user"
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
