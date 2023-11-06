7c7960c0863b1340469569856c7f12e750c2d4ef
sungno <ysn3939@gmail.com>
2023-11-06 11:19:30 +0900

    django course 33 commit

--- profileapp/views.py
+++ profileapp/views.py
@@ -8,13 +8,12 @@ from django.views.generic import CreateView, UpdateView
 from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
-
+from django.urls import reverse
 
 class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = "target_profile"
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
     ###
@@ -24,6 +23,8 @@ class ProfileCreateView(CreateView):
         temp_profile.save()
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -31,7 +32,8 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = "target_profile"
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
 
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
 
