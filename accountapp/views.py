63fb0449d0269b9563e929a2890f1450d4dadf6b
sungno <ysn3939@gmail.com>
2023-11-07 16:28:44 +0900

    django course 41 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -8,12 +8,15 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, DeleteView, UpdateView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
+from articleapp.models import Article
 
 has_ownership = [account_ownership_required, login_required]
+
 
 @login_required
 def hello_world(request):
@@ -28,6 +31,7 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
+
 # user 를 위한 클래스
 class AccountCreateView(CreateView):
     model = User
@@ -36,11 +40,16 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
 
 
-
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = "target_user"
     template_name = 'accountapp/detail.html'
+
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
 
 @method_decorator(has_ownership, 'get')
